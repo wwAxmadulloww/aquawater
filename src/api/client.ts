567507fb6071e352e715bcd7c0a9283e1a5f8add
Axmadullo <axmadullo@MacBook-Air-Axmadullo.local>
2026-03-09 17:24:03 +0500

    fix api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE_API_BASE_URL}/api` : '/api',
     headers: { 'Content-Type': 'application/json' },
 })
 
